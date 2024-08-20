e6e3380aeaa114d8705ea9664654b42ba21c1006
Naushikha Jayawickrama <dev@naushikha.com>
2024-08-20 11:05:27 +0530

    Add proper time logging start + comment

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -17,7 +17,7 @@ import {
 } from "@mui/material";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import dayjs, { Dayjs } from "dayjs";
 import "dayjs/locale/en-gb";
 import SettingsDialog from "./Settings";
@@ -189,7 +189,8 @@ const TaskForm: React.FC = () => {
   const addWorklogToJiraItem = async (
     issueId: string,
     taskDataDate: Dayjs,
-    hours: number
+    hours: number,
+    comment: string
   ) => {
     const headers = getJiraAPIHeaders();
     const date = taskDataDate.format("YYYY/MM/DD");
@@ -201,7 +202,7 @@ const TaskForm: React.FC = () => {
     jiraAPIDate =
       jiraAPIDate.split("+")[0] + ".000+" + jiraAPIDate.split("+")[1];
     const worklogData = {
-      comment: `Worked ${hours}h on ${date}`,
+      comment: comment,
       started: jiraAPIDate,
       timeSpent: `${hours}h`,
     };
@@ -395,7 +396,8 @@ const TaskForm: React.FC = () => {
       await addWorklogToJiraItem(
         taskDataItem.task.key,
         taskDataItem.date,
-        taskDataItem.hours
+        taskDataItem.hours,
+        comment
       );
     }
     setSubmissionInProgress(false);
@@ -430,13 +432,14 @@ const TaskForm: React.FC = () => {
               dateAdapter={AdapterDayjs}
               adapterLocale="en-gb"
             >
-              <DatePicker
+              <DateTimePicker
                 disableFuture
                 label="Select Date"
                 value={selectedDate}
-                views={["year", "month", "day"]}
-                format="YYYY/MM/DD"
+                views={["year", "month", "day", "hours", "minutes"]}
+                format="YYYY/MM/DD hh:mma"
                 onChange={handleDateChange}
+                ampm
               />
             </LocalizationProvider>
           </Grid>
@@ -541,9 +544,9 @@ const TaskForm: React.FC = () => {
                   {taskData.map((taskDataItem) => (
                     <TableRow key={taskDataItem.id}>
                       <TableCell align="left">
-                        {taskDataItem.date.year()} /{" "}
-                        {String(taskDataItem.date.month() + 1).padStart(2, "0")}{" "}
-                        / {String(taskDataItem.date.date()).padStart(2, "0")}
+                        {taskDataItem.date.format("YYYY/MM/DD")}
+                        <br />
+                        {taskDataItem.date.format("hh:mma")}
                       </TableCell>
                       <TableCell align="center">
                         [{taskDataItem.task.key}] {taskDataItem.task.title}
